9b373bbe5147df9a6640d6d18447ed507c072a77
Domenico Capalbo <capalbodomenico@gmail.com>
2020-11-20 23:29:56 +0100

    added-buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,81 +10,81 @@
                 <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                    <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <small>Almost</small>
+                    <a href={{asset("https://www.beatport.com/release/almost/2778381")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
                 <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                    <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <small>Follia</small>
+                    <a href={{asset("https://www.beatport.com/track/follia-original-mix/12822907")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li> 
+                    <div class="music_item_soundwave"></div> 
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
                 <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <small>Maserati</small>
+                    <a href={{asset("https://www.beatport.com/track/maserati-original-mix/12822908")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
                     <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
                 <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <small>the Clouds</small>
+                    <a href={{asset("https://www.beatport.com/track/the-clouds-original-mix/12822909")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
                     <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
-                <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
+                <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <small>Strange Time</small>
+                    <a href={{asset("https://www.beatport.com/release/strange-time/2778403")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
                     <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
-                <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
+                <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <small>Moviment</small>
+                    <a href={{asset("https://www.beatport.com/track/moviment-original-mix/12823057")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
                     <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
-                <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
+                <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <small>Mexico</small>
+                    <a href={{asset("https://www.beatport.com/track/mexico-original-mix/12823056")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
                     <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
             <div class="music_item">
-                <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
+                <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
                 <div class="music_item_info">
                     <h5>Daniel Dee</h5>
-                    <small>almost</small>
-                    <a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
+                    <small>Stricken</small>
+                    <a href={{asset("https://www.beatport.com/track/stricken-original-mix/12823059")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
                     <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
+                    <div class="music_item_soundwave"></div>
                 </div>
-                <div class="music_item_soundwave"></div>
             </div>
         </div>
     </section>
