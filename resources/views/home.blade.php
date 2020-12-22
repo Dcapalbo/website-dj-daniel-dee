556a3ee9dd7105c7b0f2fbb67845076a12120771
Domenico Capalbo <capalbodomenico@gmail.com>
2020-12-22 20:01:26 +0100

    music-embedde-beatport-complete-main-section-responsive
    website-daniel-dee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,106 +91,35 @@
     <div class="main_wrapper">
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Almost</small>
-                <a href={{asset("https://www.beatport.com/release/almost/2778381")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>
-                {{-- music item info  --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12822906&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item --}}
         <div class="music_item">
-            <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Follia</small>
-                <a href={{asset("https://www.beatport.com/track/follia-original-mix/12822907")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li> 
-                <div class="music_item_soundwave"></div> 
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12822907&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Maserati</small>
-                <a href={{asset("https://www.beatport.com/track/maserati-original-mix/12822908")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
-                {{-- music item soundwave  --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12822908&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/almost_album_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>the Clouds</small>
-                <a href={{asset("https://www.beatport.com/track/the-clouds-original-mix/12822909")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
-                {{-- music item soundwave  --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12822909&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Strange Time</small>
-                <a href={{asset("https://www.beatport.com/release/strange-time/2778403")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
-                {{-- music item soundwave  --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12823058&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Moviment</small>
-                <a href={{asset("https://www.beatport.com/track/moviment-original-mix/12823057")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
-                {{-- music item soundwave  --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12823057&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Mexico</small>
-                <a href={{asset("https://www.beatport.com/track/mexico-original-mix/12823056")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li>  
-                {{-- music item soundwave  --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12823056&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
         {{-- music item  --}}
         <div class="music_item">
-            <img src="{{asset('img/strange_time_daniel_dee.jpg')}}" alt="Album_image">
-            {{-- music item info  --}}
-            <div class="music_item_info">
-                <h5>Daniel Dee</h5>
-                <small>Stricken</small>
-                <a href={{asset("https://www.beatport.com/track/stricken-original-mix/12823059")}} target="_blank"><i class="fas fa-shopping-cart"></i></a>
-                <li><a href={{asset("https://www.mixcloud.com/daniel-dee/")}} target="_blank"><i class="fab fa-mixcloud"></i></a></li> 
-                {{-- music item soundwave   --}}
-                <div class="music_item_soundwave"></div>
-            </div>
+            <iframe src="https://embed.beatport.com/?id=12823059&type=track" width="400" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
         </div>
     </div>
 </section>
