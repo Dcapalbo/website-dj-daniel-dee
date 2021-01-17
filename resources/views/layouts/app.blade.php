480bed1c4a951ece3f734006433d09ec0a3b658b
Domenico Capalbo <capalbodomenico@gmail.com>
2021-01-17 13:12:09 +0100

    asset-style-js-changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+    <link rel="stylesheet" href="/css/app.css">
     <title>@yield('title', 'Daniel dee')</title>
   </head>
   <body>
@@ -17,5 +17,6 @@
     @include('partials.footer')
     {{-- ending main --}}
   </body>
-  <script src="{{asset('/js/app.js')}}"></script>
+  <script src="/js/app.js"></script>
 </html>
+{{-- end  --}}
