aff0204b4588a4bb96d2e2b1174a3345dae76842
Domenico Capalbo <capalbodomenico@gmail.com>
2021-08-30 21:51:09 +0200

    presskit-fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -30,7 +30,6 @@
             <li><a href={{asset("https://www.facebook.com/DanielDee06?locale=it_IT%2F")}} target="_blank"><i class="fab fa-facebook-square"></a></i></li>
             <li><a href={{asset("https://www.instagram.com/danieldeedj/?fbclid=IwAR0mETN4RKMcM0G1r8nJ5qTLtN7C22p7lJ5gL6OhG8CFboIU3IPgaqztUyI")}} target="_blank"><i class="fab fa-instagram"></a></i></li>
             <li><a href={{asset("https://twitter.com/DanielDee06")}} target="_blank"><i class="fab fa-twitter" ></a></i></li>
-            <li><a href={{asset("https://open.spotify.com/artist/73rTSTOwixQnWG9UkOj6QE?si=bwj3OjgpRgyP2vXgLcLrXw")}} target="_blank"><i class="fab fa-spotify"></i></a></li>
             <a class="uk-modal-close close_modal">X</a>
         </div>
       </div>
@@ -44,7 +43,7 @@
             <h3>Biography</h3>
             <p>Daniel Dee was born in 1982 in Rome, Italy. He started his career as a DJ at the age of 15 working at a famous club in Rome: the NRG (of Ciampino). At that time, the Roman club scene was dominated by the trance-progressive club genre which influenced him greatly. After his first year of performing, he was chosen as the resident DJ of the NRG club becoming one of the most important figures of the best sunday evening in Italy. In 1998 he was played regularly on “Future Zone” on Mondo Radio which was considered at the time the best progressive house radio show of the centre region of Italy. It was on air every Sunday morning from 12am to 01 pm. As time went by widening his musical knowledge he played techno/trance and started to work in music studios creating new original tracks. At the same time he continued to gain experience in studios and playing in clubs, working with other top Dj's of Italy. Most recently, Daniel Dee has put together an original and innovative project: " The Club " which is currently being proposed to the Roman club scene.</p>
             <a class="uk-modal-close close_modal absolute_close_modal">X</a>
-            <a class="download" href="{{asset("img/Daniel_dee_pressKit.zip")}}" download>Download the artist press kit</a>
+            <a class="download" href="{{asset("img/Daniel_dee_presskit.zip")}}" download>Download the artist press kit</a>
         </div>
       </div>
     </section>
