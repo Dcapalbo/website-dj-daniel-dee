78b9947428efd48f55cb536e34af156478fa5ab7
Domenico Capalbo <omar.capalbo@interlogica.it>
2023-11-11 17:57:13 +0100

    removing useless import

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,6 @@
 
 import '../style/common.scss';
 
-import PicturesSwiper from '../components/pictureSwiper/PictureSwiper';
 import Channels from '../components/channels/channels';
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
