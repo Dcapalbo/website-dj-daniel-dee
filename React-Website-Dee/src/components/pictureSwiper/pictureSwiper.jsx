cd5951731bdb06ce7d22340db4c6458bc434dd3f
Domenico Capalbo <omar.capalbo@interlogica.it>
2023-11-11 14:15:20 +0100

    swiper and style complete

--- React-Website-Dee/src/components/pictureSwiper/pictureSwiper.jsx
+++ React-Website-Dee/src/components/pictureSwiper/pictureSwiper.jsx
@@ -3,15 +3,24 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Backdrop from '@mui/material/Backdrop';
+import { useEffect } from 'react';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
+import './pictureSwiper.scss';
 import 'swiper/css';
 
 const PicturesSwiper = ({ onClose, open }) => {
 	const handleClose = () => {
+		document.body.classList.remove('no-scroll');
 		onClose();
 	};
+
+	useEffect(() => {
+		if (open) {
+			document.body.classList.add('no-scroll');
+		}
+	}, [open]);
 
 	return (
 		<Backdrop
@@ -24,32 +33,37 @@ const PicturesSwiper = ({ onClose, open }) => {
 				pagination={{ clickable: true }}>
 				<SwiperSlide>
 					<img
+						className='swiper_single_image'
 						src='src\assets\picture_dd_1.jpg'
 						alt='picture_dd_1'
 					/>
 				</SwiperSlide>
 				<SwiperSlide>
 					<img
+						className='swiper_single_image'
 						src='src\assets\picture_dd_2.jpg'
 						alt='picture_dd_2'
 					/>
 				</SwiperSlide>
 				<SwiperSlide>
 					<img
+						className='swiper_single_image'
 						src='src\assets\picture_dd_3.jpg'
 						alt='picture_dd_3'
 					/>
 				</SwiperSlide>
 				<SwiperSlide>
 					<img
+						className='swiper_single_image'
 						src='src\assets\picture_dd_6.jpg'
 						alt='picture_dd_6'
 					/>
 				</SwiperSlide>
 				<SwiperSlide>
 					<img
-						src='src\assets\picture_dd_4.jpg'
-						alt='picture_dd_4'
+						className='swiper_single_image'
+						src='src\assets\picture_dd_5.jpg'
+						alt='picture_dd_5'
 					/>
 				</SwiperSlide>
 			</Swiper>
