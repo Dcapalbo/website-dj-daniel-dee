57fbb64b7b61cde05f59ccd787abec5fc19351f3
Domenico Capalbo <omar.capalbo@interlogica.it>
2023-11-16 03:34:43 +0100

    feat(musics component): adding title property, to embedded audio iframes

--- src/components/musics/musics.jsx
+++ src/components/musics/musics.jsx
@@ -14,6 +14,7 @@ const MusicItem = ({ trackId }) => (
 				trackId === 4263979 ? 'release' : 'track'
 			}`}
 			width='400'
+			title={trackId}
 			height='162'
 			frameBorder='0'
 			scrolling='no'
