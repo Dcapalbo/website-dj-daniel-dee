7317a3b736abd11a9b7739ce66ccc0b4b5580bb5
Dcapalbo <E5871121>
2026-03-01 07:11:26 +0200

    adding new changes for website

--- src/components/dialogs/bioDialogs/bioDialogs.jsx
+++ src/components/dialogs/bioDialogs/bioDialogs.jsx
@@ -1,25 +1,25 @@
 /** @format */
 
-import pressKit from '../../../assets/daniel_dee_presskit.pdf';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
-import Dialog from '@mui/material/Dialog';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
+import pressKit from '../../../assets/Daniel_dee_presskit_Febbraio_2026.pdf'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import CloseIcon from '@mui/icons-material/Close'
+import Dialog from '@mui/material/Dialog'
+import Button from '@mui/material/Button'
+import List from '@mui/material/List'
 
 const biographyContent = [
-	{
-		title: 'Music',
-		content: 'Progressive/Techno/Electronic Music',
-	},
-	{
-		title: 'Nationality',
-		content: 'Italian',
-	},
-	{
-		title: 'Biography',
-		content: `
+  {
+    title: 'Music',
+    content: 'Progressive/Techno/Electronic Music',
+  },
+  {
+    title: 'Nationality',
+    content: 'Italian',
+  },
+  {
+    title: 'Biography',
+    content: `
       Daniel Dee was born in 1982 in Rome, Italy. He started his career as a
       DJ at the age of 15 working at a famous club in Rome: NRG (of Ciampino).
       At that time, the Roman best club scene was dominated by
@@ -39,120 +39,124 @@ const biographyContent = [
       projects like: "The Club" and "Khoema", which are currently being
       proposed to the Roman club scene.
     `,
-	},
-];
+  },
+]
 
 const BioDialog = ({ onClose, open }) => {
-	const handleClose = () => {
-		onClose();
-	};
+  const handleClose = () => {
+    onClose()
+  }
 
-	return (
-		<Dialog
-			onClose={handleClose}
-			open={open}
-			sx={{
-				bottom: 'none',
-				top: 'auto',
-				'@media (max-width: 767px)': {
-					overflowY: 'auto',
-					top: 'none',
-					m: 0,
-					p: 0,
-				},
-			}}>
-			<IconButton
-				onClick={handleClose}
-				sx={{
-					backgroundColor: '#C41C1C',
-					position: 'absolute',
-					color: '#fff',
-					zIndex: 1400,
-					right: '8px',
-					top: '8px',
-					'&:hover': {
-						backgroundColor: '#C41C1C',
-						transform: 'scale(1.05)',
-						transition: 'background-color 0.3s, transform 0.3s',
-					},
-					'@media (max-width: 767px)': {
-						width: 30,
-						height: 30,
-					},
-				}}>
-				<CloseIcon
-					sx={{
-						'@media (max-width: 767px)': {
-							width: 25,
-						},
-					}}
-				/>
-			</IconButton>
-			<List
-				sx={{
-					flexDirection: 'column',
-					background: '#b0afb0',
-					textAlign: 'center',
-					display: 'flex',
-					color: '#000',
-					p: 2.5,
-					'@media (max-width: 767px)': {
-						p: 1.8,
-					},
-				}}>
-				{biographyContent.map((item, index) => (
-					<div key={index}>
-						<Typography
-							variant='h6'
-							sx={{
-								mb: 1,
-								fontWeight: 'bold',
-								'@media (max-width: 767px)': {
-									fontSize: 15,
-									mb: 0,
-								},
-							}}>
-							{item.title}
-						</Typography>
-						<Typography
-							variant='body1'
-							sx={{
-								mt: 1,
-								'@media (max-width: 767px)': {
-									fontSize: 12,
-									mt: 0,
-								},
-							}}>
-							{item.content}
-						</Typography>
-					</div>
-				))}
-				<div sx={{ mt: 2 }}>
-					<Typography
-						variant='body1'
-						sx={{ mt: 1, fontWeight: 'bold' }}>
-						Download the presskit
-					</Typography>
-					<Button
-						variant='contained'
-						color='primary'
-						size='small'
-						sx={{
-							mt: 1,
-							'&:hover': {
-								backgroundColor: '#C41C1C',
-								transform: 'scale(1.05)',
-								transition: 'background-color 0.3s, transform 0.3s',
-							},
-						}}
-						href={pressKit}
-						download>
-						Download
-					</Button>
-				</div>
-			</List>
-		</Dialog>
-	);
-};
+  return (
+    <Dialog
+      onClose={handleClose}
+      open={open}
+      sx={{
+        bottom: 'none',
+        top: 'auto',
+        '@media (max-width: 767px)': {
+          overflowY: 'auto',
+          top: 'none',
+          m: 0,
+          p: 0,
+        },
+      }}
+    >
+      <IconButton
+        onClick={handleClose}
+        sx={{
+          backgroundColor: '#C41C1C',
+          position: 'absolute',
+          color: '#fff',
+          zIndex: 1400,
+          right: '8px',
+          top: '8px',
+          '&:hover': {
+            backgroundColor: '#C41C1C',
+            transform: 'scale(1.05)',
+            transition: 'background-color 0.3s, transform 0.3s',
+          },
+          '@media (max-width: 767px)': {
+            width: 30,
+            height: 30,
+          },
+        }}
+      >
+        <CloseIcon
+          sx={{
+            '@media (max-width: 767px)': {
+              width: 25,
+            },
+          }}
+        />
+      </IconButton>
+      <List
+        sx={{
+          flexDirection: 'column',
+          background: '#b0afb0',
+          textAlign: 'center',
+          display: 'flex',
+          color: '#000',
+          p: 2.5,
+          '@media (max-width: 767px)': {
+            p: 1.8,
+          },
+        }}
+      >
+        {biographyContent.map((item, index) => (
+          <div key={index}>
+            <Typography
+              variant='h6'
+              sx={{
+                mb: 1,
+                fontWeight: 'bold',
+                '@media (max-width: 767px)': {
+                  fontSize: 15,
+                  mb: 0,
+                },
+              }}
+            >
+              {item.title}
+            </Typography>
+            <Typography
+              variant='body1'
+              sx={{
+                mt: 1,
+                '@media (max-width: 767px)': {
+                  fontSize: 12,
+                  mt: 0,
+                },
+              }}
+            >
+              {item.content}
+            </Typography>
+          </div>
+        ))}
+        <div sx={{ mt: 2 }}>
+          <Typography variant='body1' sx={{ mt: 1, fontWeight: 'bold' }}>
+            Download the presskit
+          </Typography>
+          <Button
+            variant='contained'
+            color='primary'
+            size='small'
+            sx={{
+              mt: 1,
+              '&:hover': {
+                backgroundColor: '#C41C1C',
+                transform: 'scale(1.05)',
+                transition: 'background-color 0.3s, transform 0.3s',
+              },
+            }}
+            href={pressKit}
+            download
+          >
+            Download
+          </Button>
+        </div>
+      </List>
+    </Dialog>
+  )
+}
 
-export default BioDialog;
+export default BioDialog
--- src/components/musics/musics.jsx
+++ src/components/musics/musics.jsx
@@ -1,37 +1,35 @@
 /** @format */
 
-import './musics.scss';
+import './musics.scss'
 
-const musicItems = [4743163, 4694102, 4578126, 4522908, 4423253, 4336789, 4263979, 18059075, 17906123, 17780405];
-const releasedTracks = [4743163, 4694102, 4578126, 4522908, 4423253, 4336789, 4263979];
+const musicItems = [5570773, 5395643, 5330008, 5017016, 4900595]
+const releasedTracks = [5570773, 5395643, 5330008, 5017016, 4900595]
 
 const MusicItem = ({ trackId }) => (
-	<div className='music_item'>
-		<iframe
-			src={`https://embed.beatport.com/?id=${trackId}&type=${releasedTracks.includes(trackId) ? 'release' : 'track'}`}
-			width='400'
-			title={trackId}
-			height='162'
-			frameBorder='0'
-			scrolling='no'
-			style={{ maxWidth: '600px' }}></iframe>
-	</div>
-);
+  <div className='music_item'>
+    <iframe
+      src={`https://embed.beatport.com/?id=${trackId}&type=${releasedTracks.includes(trackId) ? 'release' : 'track'}`}
+      width='400'
+      title={trackId}
+      height='162'
+      frameBorder='0'
+      scrolling='no'
+      style={{ maxWidth: '600px' }}
+    ></iframe>
+  </div>
+)
 
 const Musics = () => {
-	return (
-		<section className='second_main_section'>
-			<h1 id='landing_music'>Music</h1>
-			<div className='main_wrapper'>
-				{musicItems.map((trackId, index) => (
-					<MusicItem
-						key={index}
-						trackId={trackId}
-					/>
-				))}
-			</div>
-		</section>
-	);
-};
+  return (
+    <section className='second_main_section'>
+      <h1 id='landing_music'>Music</h1>
+      <div className='main_wrapper'>
+        {musicItems.map((trackId, index) => (
+          <MusicItem key={index} trackId={trackId} />
+        ))}
+      </div>
+    </section>
+  )
+}
 
-export default Musics;
+export default Musics
